7fb34bd31ea75c585e2fc4d68e6788fa3f1b43ce
JoaopedroHZN <joaopedromontelo164@gmail.com>
2026-05-29 09:14:59 -0300

    Order Repository, Resource and Service

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -19,8 +20,10 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "client") // Um para muitos
     private List<Order> orders = new ArrayList<>(); // O cliente tem varios pedidos
+
 
     public User() {
     }
